3945d95f1e4515c45ccda802e512dbb5a1fa8ed0
Abdullah Alnuaimi <abdullahalnuaimi36@gmail.com>
2021-03-07 00:07:59 +0300

    Finish Firebase Authentication Section

--- lib/app/sign_in/sign_in_page.dart
+++ lib/app/sign_in/sign_in_page.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
 
+import 'package:firebase_auth/firebase_auth.dart';
+
 import 'package:time_tracker_flutter/app/sign_in/sign_in_button.dart';
 import 'package:time_tracker_flutter/app/sign_in/social_sign_in_button.dart';
 
 class SignInPage extends StatelessWidget {
+  Future<void> _signInAnonymously() async {
+    try {
+      final userCredential = await FirebaseAuth.instance.signInAnonymously();
+      print('${userCredential.user?.uid}');
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,72 +26,72 @@ class SignInPage extends StatelessWidget {
       body: _buildContent(),
     );
   }
-}
 
-Widget _buildContent() {
-  return Container(
-    child: Column(
-      children: [
-        Text(
-          'Sign in',
-          style: TextStyle(
-            fontSize: 32.0,
-            fontWeight: FontWeight.w600,
+  Widget _buildContent() {
+    return Container(
+      child: Column(
+        children: [
+          Text(
+            'Sign in',
+            style: TextStyle(
+              fontSize: 32.0,
+              fontWeight: FontWeight.w600,
+            ),
+            textAlign: TextAlign.center,
           ),
-          textAlign: TextAlign.center,
-        ),
-        SizedBox(
-          height: 50.0,
-        ),
-        SocialSignInButton(
-          assetName: 'images/google_logo.png',
-          color: Colors.white,
-          text: 'Sign in with Google',
-          textColor: Colors.black,
-          onPressed: () {},
-        ),
-        SizedBox(
-          height: 10.0,
-        ),
-        SocialSignInButton(
-          assetName: 'images/facebook_logo.png',
-          color: Color(0xFF334D92),
-          text: 'Sign in with Facebook',
-          textColor: Colors.white,
-          onPressed: () {},
-        ),
-        SizedBox(
-          height: 10.0,
-        ),
-        SignInButton(
-          color: Colors.teal[700],
-          text: 'Sign in with Email',
-          textColor: Colors.white,
-          onPressed: () {},
-        ),
-        SizedBox(
-          height: 10.0,
-        ),
-        Text(
-          'or',
-          style: TextStyle(
-            fontSize: 16.0,
+          SizedBox(
+            height: 50.0,
           ),
-          textAlign: TextAlign.center,
-        ),
-        SizedBox(
-          height: 10.0,
-        ),
-        SignInButton(
-          color: Colors.lime[400],
-          text: 'Go anonymous',
-          textColor: Colors.black,
-          onPressed: () {},
-        ),
-      ],
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      mainAxisAlignment: MainAxisAlignment.center,
-    ),
-    padding: EdgeInsets.all(16.0),
-  );
+          SocialSignInButton(
+            assetName: 'images/google_logo.png',
+            color: Colors.white,
+            text: 'Sign in with Google',
+            textColor: Colors.black,
+            onPressed: () {},
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          SocialSignInButton(
+            assetName: 'images/facebook_logo.png',
+            color: Color(0xFF334D92),
+            text: 'Sign in with Facebook',
+            textColor: Colors.white,
+            onPressed: () {},
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          SignInButton(
+            color: Colors.teal[700],
+            text: 'Sign in with Email',
+            textColor: Colors.white,
+            onPressed: () {},
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          Text(
+            'or',
+            style: TextStyle(
+              fontSize: 16.0,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          SignInButton(
+            color: Colors.lime[400],
+            text: 'Go anonymous',
+            textColor: Colors.black,
+            onPressed: _signInAnonymously,
+          ),
+        ],
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.center,
+      ),
+      padding: EdgeInsets.all(16.0),
+    );
+  }
 }
