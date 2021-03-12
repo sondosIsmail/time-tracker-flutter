698602e66bcf1218df820a78d570c1d5cfe0eabb
Abdullah Alnuaimi <abdullahalnuaimi36@gmail.com>
2021-03-12 13:25:17 +0300

    Fix Facebook Login Errors

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/services.dart';
 
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter_facebook_login/flutter_facebook_login.dart';
+import 'package:flutter_login_facebook/flutter_login_facebook.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 abstract class AuthBase {
@@ -16,37 +16,40 @@ class Auth implements AuthBase {
   final _firebaseAuth = FirebaseAuth.instance;
 
   @override
-  Stream<User?> get authStateChanges {
-    return _firebaseAuth.authStateChanges();
-  }
+  Stream<User?> get authStateChanges => _firebaseAuth.authStateChanges();
 
   @override
   Future<User?> signInAnonymously() async {
-    try {
-      final UserCredential userCredential =
-          await _firebaseAuth.signInAnonymously();
-      return userCredential.user;
-    } catch (e) {
-      print(e.toString());
-    }
+    final userCredential = await _firebaseAuth.signInAnonymously();
+    return userCredential.user;
   }
 
   @override
   Future<User?> signInWithFacebook() async {
-    final facebookLogin = FacebookLogin();
-    final loginResult = await facebookLogin.logInWithReadPermissions(
-      ['public_profile'],
-    );
-    if (loginResult.accessToken != null) {
-      final authResult = await _firebaseAuth.signInWithCredential(
-        FacebookAuthProvider.credential(loginResult.accessToken.token),
-      );
-      return authResult.user;
-    } else {
-      throw PlatformException(
-        code: 'ERROR_MISSING_Facebook_AUTH_TOKEN',
-        message: 'Missing facebook auth token',
-      );
+    final fb = FacebookLogin();
+    final response = await fb.logIn(permissions: [
+      FacebookPermission.publicProfile,
+      FacebookPermission.email,
+    ]);
+    switch (response.status) {
+      case FacebookLoginStatus.success:
+        final accessToken = response.accessToken;
+        final userCredential = await _firebaseAuth.signInWithCredential(
+          FacebookAuthProvider.credential(accessToken!.token),
+        );
+        return userCredential.user;
+      case FacebookLoginStatus.cancel:
+        throw FirebaseAuthException(
+          code: 'ERROR_ABORTED_BY_USER',
+          message: 'Sign in aborted by user',
+        );
+      case FacebookLoginStatus.error:
+        throw FirebaseAuthException(
+          code: 'ERROR_FACEBOOK_LOGIN_FAILED',
+          message: response.error!.developerMessage,
+        );
+      default:
+        throw UnimplementedError();
     }
   }
 
@@ -58,7 +61,7 @@ class Auth implements AuthBase {
     if (googleAccount != null) {
       final googleAuth = await googleAccount.authentication;
 
-      if (googleAuth.accessToken != null && googleAuth.idToken != null) {
+      if (googleAuth.idToken != null) {
         final authResult = await _firebaseAuth.signInWithCredential(
           GoogleAuthProvider.credential(
             accessToken: googleAuth.accessToken,
